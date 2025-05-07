9e2745a44e067122826980665ce5bfd98d2c9a88
sadaan <ansarisadaan72@gmail.com>
2025-05-07 23:34:15 +0530

    "created feed component"

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { BASE_URL } from '../utils/constants'
+import { useDispatch, useSelector } from 'react-redux'
+import { addFeed } from '../utils/feedSlice'
+import UserCard from './userCard'
 
 const Feed = () => {
+    const feed = useSelector((store) => store.feed);
+    const dispatch = useDispatch();
+
+    const getFeed = async () => {
+        if (feed) return;
+        try {
+            const response = await axios.get(BASE_URL + "/feed", { withCredentials: true })
+            console.log(response);
+            dispatch(addFeed(response?.data?.data));
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        getFeed();
+    }, [])
+
     return (
-        <div>Feed</div>
+        feed && (<div className='flex justify-center my-10'>            
+                <UserCard  user={feed[0]} />
+        </div>)
     )
 }
 
