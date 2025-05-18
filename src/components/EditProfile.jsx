10b044eb89330871a29434da8f3cd9723856123a
sadaan <ansarisadaan72@gmail.com>
2025-05-18 20:43:47 +0530

    minor chnages for dispatch data

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -1,37 +1,37 @@
 import React, { useState } from 'react'
 import UserCard from './userCard';
 import axios from 'axios';
-import { BASE_URL } from '../utils/constants';
+// import { BASE_URL } from '../utils/constants';
 import { useDispatch } from 'react-redux';
 import { addUser } from '../utils/userSlice';
 
 const EditProfile = ({ user }) => {
 
-    const [firstName, setFirstName] = useState(user?.data?.firstName);
-    const [lastName, setLastName] = useState(user?.data?.lastName);
-    const [age, setAge] = useState(user?.data?.age);
-    const [gender, setGender] = useState(user?.data?.gender);
-    const [about, setAbout] = useState(user?.data?.about);
-    const [photoUrl, setPhotoUrl] = useState(user?.data?.photoUrl);
+    const [firstName, setFirstName] = useState(user?.firstName);
+    const [lastName, setLastName] = useState(user?.lastName);
+    const [age, setAge] = useState(user?.age);
+    const [gender, setGender] = useState(user?.gender);
+    const [about, setAbout] = useState(user?.about);
+    const [photoUrl, setPhotoUrl] = useState(user?.photoUrl);
     const [error, setError] = useState('');
     const dispatch = useDispatch();
 
     const saveProfile = async () => {
         try {
-            const response = await axios.patch(BASE_URL + "/profile/edit",
+            const response = await axios.patch("/api/v1/users/profile/edit",
                 {
                     firstName,
                     lastName,
-                    age,
+                    age: Number(age),
                     gender,
                     about,
                     photoUrl
                 },
                 { withCredentials: true }
             )
-            dispatch(addUser(response));
+            dispatch(addUser(response?.data?.data));
         } catch (error) {
-            setError(error.message);
+            setError(error.response?.data?.message || error.message);
         }
     }
 
