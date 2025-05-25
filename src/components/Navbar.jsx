aca8c525648ef936ea7094140512e966da6f7ec5
sadaan <ansarisadaan72@gmail.com>
2025-05-25 15:22:05 +0530

    "added connection page->link"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
                                 <span className="badge">New</span>
                             </Link>
                         </li>
-                        <li><a>Settings</a></li>
+                        <li><Link to={'/connections'}>Connections</Link></li>
                         <li><Link onClick={handleLogout}>Logout</Link></li>
                     </ul>
                 </div>
