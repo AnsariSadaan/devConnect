497adec01d9e0ac487c11d9f3e613d3cc5b11d1c
sadaan <ansarisadaan72@gmail.com>
2025-06-15 01:43:21 +0530

    added user store and improve error handling

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,24 +3,26 @@ import Navbar from './Navbar'
 import { Outlet, useNavigate } from 'react-router-dom'
 import Footer from './Footer'
 import { BASE_URL } from '../utils/constants'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { addUser } from '../utils/userSlice'
 import axios from 'axios'
 
 const Body = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const userData = useSelector((store) => store.user);
     const fetchUser = async () => {
+        if (userData) return;
         try {
             const res = await axios.get(BASE_URL + "/profile/view", {
                 withCredentials: true,
             });
             dispatch(addUser(res.data.data));
         } catch (error) {
-            if(error.status === 401){
+            if (error.response && error.response.status === 401){
                 navigate("/login")
             }
-            console.error(error);
+            console.error("Unexpected error:", error);
         }
     }
 
