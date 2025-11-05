e74c4ed5a97b8fabb01d9bfbebb1723a8a8a2044
Sadaan Ansari <ansarisadaan72@gmail.com>
2025-11-05 22:29:35 +0530

    [update]::: fetching chats from db

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,42 +2,76 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { createSocketConnection } from '../utils/socket.js';
 import { useSelector } from 'react-redux';
+import axios from 'axios';
+import { BASE_URL } from '../utils/constants.js';
 
 const Chat = () => {
   const {targetUserId} = useParams();
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState("")
   const user =  useSelector(store => store.user);
+  const [error, setError] = useState('');
+  const [socket, setSocket] = useState(null);
   const userId = user?._id;
   // console.log(targetUserId);
 
+  const fetchChatMesages = async ()=> {
+    try {
+      const chat = await axios.get(BASE_URL + "/chat/"+ targetUserId, {
+        withCredentials: true
+      });
+
+      console.log(chat.data.data.messages);
+
+      const chatMessages = chat?.data?.data?.messages.map((msg) => {
+        const {senderId, text} = msg;
+        return {
+          firstName: senderId?.firstName,
+          lastName: senderId?.lastName,
+          text
+        };
+      })
+      setMessages(chatMessages);
+
+    } catch (error) {
+      setError(error.response || error.message);
+    }
+  } 
+
+  useEffect(()=> {
+    fetchChatMesages();
+  }, [])
 
   useEffect( ()=> {
     if(!userId || !targetUserId){
       return;
     }
-    const socket = createSocketConnection();
-    socket.emit('joinChat', {
+    const newSocket = createSocketConnection();
+    setSocket(newSocket);
+
+    newSocket.emit('joinChat', {
       firstName: user.firstName, 
+      lastName: user.lastName,
       userId, 
       targetUserId
     });
 
-    socket.on("messageReceived", ({ firstName, text }) => {
-      console.log(firstName + " : " + text);
-      setMessages((messages) => [...messages, {firstName, text}])
+    newSocket.on("messageReceived", ({ firstName, lastName, text }) => {
+      console.log(firstName +" " + lastName + " : " + text);
+      setMessages((messages) => [...messages, {firstName, lastName, text}])
     });
 
     return () => {
       console.log("socket disconnected");
-      socket.disconnect();
+      newSocket.disconnect();
     }
   }, [userId, targetUserId]);
 
   const sendMessage = () => {
-    const socket = createSocketConnection();
+    if (!socket) return;
     socket.emit('sendMessage', {
       firstName: user.firstName, 
+      lastName: user.lastName,
       userId, 
       targetUserId, 
       text: newMessage
@@ -53,7 +87,7 @@ const Chat = () => {
           return (
             <div key={index} className="chat chat-start">
               <div className="chat-header">
-                {msg.firstName}
+                {`${msg.firstName} ${msg.lastName}`}
                 <time className="text-xs opacity-50">2 hours ago</time>
               </div>
               <div className="chat-bubble">{msg.text}</div>
