d23d4c418798f35d73e4add57b3d12ae3222bfc3
Sadaan Ansari <ansarisadaan72@gmail.com>
2026-06-26 16:50:42 +0530

    [update]::: formatted payment schema

--- src/models/payment.model.js
+++ src/models/payment.model.js
@@ -1,47 +1,67 @@
 import mongoose from "mongoose";
 
 const paymentSchema = new mongoose.Schema({
-    userId: {
-        type: mongoose.Types.ObjectId,
-        ref: "User",
-        required: true
+  userId: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  paymentId: {
+    type: String,
+    // required: true,
+  },
+  orderId: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: [
+      "created",
+      "paid",
+      "failed",
+      "refunded"
+    ]
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  provider: {
+    type: String,
+    default: "razorpay"
+  },
+  currency: {
+    type: String,
+    required: true,
+  },
+  receipt: {
+    type: String,
+    required: true,
+  },
+  notes: {
+    firstName: {
+      type: String,
     },
-    paymentId: {
-        type: String,
-        // required: true,
+    lastName: {
+      type: String,
     },
-    orderId: {
-        type: String,
-        required: true,
-    },
-    status: {
-        type: String,
-        required: true,
-    },
-    amount: {
-        type: Number,
-        required: true,
-    },
-    currency: {
-        type: String,
-        required: true,
-    },
-    receipt: {
-        type: String,
-        required: true,
-    },
-    notes: {
-        firstName: {
-            type: String,
-        },
-        lastName: {
-            type: String,
-        },
-        membershipType: {
-            type: String,
-        }
+    membershipType: {
+      type: String,
     }
-}, {timestamps: true});
+  }
+}, {
+    timestamps: true
+  } 
+);
 
-
+paymentSchema.index(
+  {
+    orderId: 1
+  },
+  {
+    unique: true
+  }
+);
 export const Payment = mongoose.model("Payment", paymentSchema); 
