0e0f637a68cbf4b0fd9bfde50c9d2e954788153c
Sadaan Ansari <ansarisadaan72@gmail.com>
2026-06-26 16:44:51 +0530

    [update]::: improved user schema and as per model improved controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,77 +4,80 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { AsyncHandler } from "../utils/AsyncHandler.js";
 
-const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
+const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills location currentCompany experience";
 
 const Feed = AsyncHandler(async (req, res) => {
-    const loggedInUser = req.user;
+  
+  const loggedInUser = req.user;
 
-    //find all connection requests involving the logged-in user
-    const sentConnectionrequests = await ConnectionRequest.find({
-        $or: [
-            { fromUserId: loggedInUser._id },
-            { toUserId: loggedInUser._id },
-        ],
-        status: { $in: ["interested", "accepted"] },
-    });
+  //find all connection requests involving the logged-in user
+  const sentConnectionrequests = await ConnectionRequest.find({
+    $or: [
+      { fromUserId: loggedInUser._id },
+      { toUserId: loggedInUser._id },
+    ],
+    status: { $in: ["interested", "accepted"] },
+  });
 
-    // exlcude user from feed who have already send request or accpted
-    const excludedUserIds = new Set();
-    sentConnectionrequests.forEach(req => {
-        excludedUserIds.add(req.fromUserId.toString());
-        excludedUserIds.add(req.toUserId.toString());
-    })
+  // exlcude user from feed who have already send request or accpted
+  const excludedUserIds = new Set();
+  sentConnectionrequests.forEach(req => {
+    excludedUserIds.add(req.fromUserId.toString());
+    excludedUserIds.add(req.toUserId.toString());
+  })
 
-    //always exclude logged in user 
-    excludedUserIds.add(loggedInUser._id.toString());
+  //always exclude logged in user 
+  excludedUserIds.add(loggedInUser._id.toString());
 
 
-    // fecth users not involved in any existing request or connection 
-    const users = await User.find({
-        _id: { $nin: Array.from(excludedUserIds) },
-    }).select(USER_SAFE_DATA)
+  // fecth users not involved in any existing request or connection 
+  const users = await User.find({
+    _id: { $nin: Array.from(excludedUserIds) },
+  }).select(USER_SAFE_DATA)
 
-    if (!users) {
-        throw new ApiError(401, "Feeds not found");
-    }
+  if (!users) {
+    throw new ApiError(401, "Feeds not found");
+  }
 
-    return res.status(200).json(new ApiResponse(200, users, "feed fetched successfully!"));
+  return res.status(200).json(new ApiResponse(200, users, "feed fetched successfully!"));
 })
 
 const userRequestsReceived = AsyncHandler(async (req, res) => {
-    const loggedInUser = req.user;
-    const connectionrequests = await ConnectionRequest.find({
-        toUserId: loggedInUser._id,
-        status: "interested",
-    }).populate("fromUserId", USER_SAFE_DATA);
+  
+  const loggedInUser = req.user;
+  const connectionrequests = await ConnectionRequest.find({
+    toUserId: loggedInUser._id,
+    status: "interested",
+  }).populate("fromUserId", USER_SAFE_DATA);
 
-    if (!connectionrequests || connectionrequests.length === 0) {
-        throw new ApiError(404, "No connection requests found")
-    }
+  if (!connectionrequests || connectionrequests.length === 0) {
+    throw new ApiError(404, "No connection requests found")
+  }
 
-    return res.status(200).json(new ApiResponse(200, connectionrequests, "Data Fetched Successfully!"))
+  return res.status(200).json(new ApiResponse(200, connectionrequests, "Data Fetched Successfully!"))
 })
 
 const userConnection = AsyncHandler(async (req, res) => {
-    const loggedInUser = req.user;
-    const connectionRequests = await ConnectionRequest.find({
-        $or: [
-            { toUserId: loggedInUser._id, status: "accepted" },
-            { fromUserId: loggedInUser._id, status: "accepted" },
-        ],
-    })
-        .populate("fromUserId", USER_SAFE_DATA)
-        .populate("toUserId", USER_SAFE_DATA);
+  
+  const loggedInUser = req.user;
+  const connectionRequests = await ConnectionRequest.find({
+    $or: [
+      { toUserId: loggedInUser._id, status: "accepted" },
+      { fromUserId: loggedInUser._id, status: "accepted" },
+    ],
+  })
+  .populate("fromUserId", USER_SAFE_DATA)
+  .populate("toUserId", USER_SAFE_DATA);
 
-    // console.log(connectionRequests);
-    if (!connectionRequests || connectionRequests.length === 0) {
-        throw new ApiError(404, "No connections found");
-    }
+  // console.log(connectionRequests);
+  if (!connectionRequests || connectionRequests.length === 0) {
+    throw new ApiError(404, "No connections found");
+  }
 
-    const data = connectionRequests.map((row) => {
-        return row.fromUserId._id.toString() === loggedInUser._id.toString() ? row.toUserId : row.fromUserId;
-    });
-    return res.status(200).json(new ApiResponse(200, data, "Connection fetched successfully"));
+  const data = connectionRequests.map((row) => {
+    return row.fromUserId._id.toString() === loggedInUser._id.toString() ? row.toUserId : row.fromUserId;
+  });
+  return res.status(200).json(new ApiResponse(200, data, "Connection fetched successfully"));
 })
 
 export { Feed, userRequestsReceived, userConnection }
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,92 +4,184 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
 
 const userSchema = new Schema(
-    {
-        firstName: {
-            type: String,
-            required: true,
-            minLength: 4,
-            maxLength: 50
-        },
-        lastName: {
-            type: String
-        },
-        emailId: {
-            type: String,
-            lowercase: true,
-            required: true,
-            unique: true,
-            trim: true,
-            validate(value) {
-                if (!validator.isEmail(value)) {
-                    throw new Error("Invalid email address: " + value);
-                }
-            }
-        },
-        password: {
-            type: String,
-            required: true,
-            validate(value) {
-                if (!validator.isStrongPassword(value)) {
-                    throw new Error("Enter a strong password: " + value);
-                }
-            }
-        },
-        age: {
-            type: Number,
-            min: 18,
-        },
-        gender: {
-            type: String,
-            enum: {
-                values: ["male", "female", "other"],
-                message: `{VALUE} is not valid gender type`,
-            }
-        },
-        isPremium: {
-            type: Boolean,
-            default: false,
-        },
-        membershipType: {
-            type: String,
-        },
-        photoUrl: {
-            type: String,
-            default: "https://geographyandyou.com/images/user-profile.png",
-            validate(value) {
-                if (!validator.isURL(value)) {
-                    throw new Error('Invalid Photo URL: ', +value);
-                }
-            }
-        },
-        about: {
-            type: String,
-            default: "thi is default about of the user!",
-        },
-        skills: {
-            type: [String],
-        },
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: 4,
+      maxLength: 50,
     },
-    { timestamps: true }
-)
+    lastName: {
+      type: String,
+      trim: true,
+      maxLength: 50,
+    },
+    emailId: {
+      type: String,
+      lowercase: true,
+      required: true,
+      unique: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid Email");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 8,
+      select: false,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Enter a strong password: " + value);
+        }
+      }
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      enum: {
+        values: ["male", "female", "other"],
+        message: `{VALUE} is not valid gender type`,
+      }
+    },
+    isPremium: {
+      type: Boolean,
+      default: false,
+    },
+    membershipType: {
+      type: String,
+      enum: ["monthly", "quarterly", "yearly"],
+    },
+    
+    premiumExpiresAt: Date,
 
+    photoUrl: {
+      type: String,
+      default: "https://geographyandyou.com/images/user-profile.png",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error(`Invalid Photo URL: ${value}`);
+        }
+      }
+    },
+    about: {
+      type: String,
+      maxlength: 500,
+      default: "This is default about of the user!",
+    },
+    skills: [
+      {
+        type: String,
+        trim: true,
+      },
+    ],
+    experience: {
+      type: Number,
+      default: 0,
+    },
+    
+    githubUrl: {
+      type: String,
+      trim: true,
+      default: "",
+      validate(value) {
+        if(value && !validator.isURL(value)) {
+          throw new Error(`Invalid github profile URL: ${value}`);
+        }
+      }
+    },
+
+    linkedinUrl: {
+      type: String,
+      trim: true,
+      default: "",
+      validate(value) {
+        if (value && !validator.isURL(value)) {
+          throw new Error(`Invalid Linkedin profile URL: ${value}`);
+        }
+      }
+    },
+
+    portfolioUrl: {
+      type: String,
+      trim: true,
+      default: "",
+      validate(value) {
+        if (value && !validator.isURL(value)) {
+          throw new Error(`Invalid portfolio profile URL: ${value}`);
+        }
+      }
+    },
+
+    location: {
+      type: String,
+      trim: true, 
+    },
+
+    currentCompany: {
+      type: String,
+      trim: true,
+    },
+
+    lastSeen: {
+      type: Date,
+      default: Date.now,
+    },
+    
+    isOnline: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { 
+    timestamps: true 
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
 userSchema.methods.getJWT = async function () {
-    const user = this;
-    const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET, {
-        expiresIn: process.env.ExpiresIn
-    });
-    return token;
+  
+  const user = this;
+  const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET, {
+    expiresIn: process.env.ExpiresIn
+  });
+  return token;
 };
 
 userSchema.methods.validatePassword = async function (passwordInputByUser) {
-    const user = this;
-    const passwordHash =  user.password;
-    const isPasswordValid = await bcrypt.compare(
-        passwordInputByUser,
-        passwordHash
+
+  console.log("Input Password:", passwordInputByUser);
+  console.log("DB Hash:", this.password);
+
+  const user = this;
+  
+  const passwordHash =  user.password;
+  if (!passwordHash) {
+    throw new Error(
+      "Password field not selected"
     );
-    return isPasswordValid;
+  }
+  const isPasswordValid = await bcrypt.compare(
+    passwordInputByUser,
+    passwordHash
+  );
+  return isPasswordValid;
 }
 
 
