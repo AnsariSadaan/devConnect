7639361de09527d0b78de074132ccc2eac2fd48b
Sadaan Ansari <ansarisadaan72@gmail.com>
2026-06-26 16:52:21 +0530

    [update]::: formatted connection schema and andd unique in index

--- src/models/connectionRequest.model.js
+++ src/models/connectionRequest.model.js
@@ -2,35 +2,44 @@ import mongoose from 'mongoose';
 import { ApiError } from '../utils/ApiError.js';
 
 const connectionRequestSchema = new mongoose.Schema({
-    fromUserId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
+  fromUserId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  toUserId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    default: "pending",
+    enum: {
+      values: ["ignored", "interested", "accepted", "rejected"],
+      message: `{VALUE} is incorrect status type`,
     },
-    toUserId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
-    },
-    status: {
-        type: String,
-        required: true,
-        enum: {
-            values: ["ignored", "interested", "accepted", "rejected"],
-            message: `{VALUE} is incorrect status type`,
-        },
-    }
+  }
 }, { timestamps: true });
 
 
-connectionRequestSchema.index({fromUserId:1, toUserId:1});
+connectionRequestSchema.index(
+  { 
+    fromUserId: 1, 
+    toUserId: 1 
+  }, 
+  {
+    unique: true,
+  }
+);
 connectionRequestSchema.pre("save", function (next) {
-    const connectionRequest = this;
-    // Check if the fromUserId is same as toUserId
-    if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)){
-        throw new ApiError(403, "cannot send connection request to yourself")
-    }
-    next();
+  const connectionRequest = this;
+  // Check if the fromUserId is same as toUserId
+  if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)){
+    throw new ApiError(403, "cannot send connection request to yourself")
+  }
+  next();
 })
 
 export const ConnectionRequest = mongoose.model("connectionRequest", connectionRequestSchema)
