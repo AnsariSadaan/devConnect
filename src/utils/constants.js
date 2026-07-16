1ae91d9e692f3e7b88836c412d3a7e724eeb4692
Sadaan Ansari <ansarisadaan72@gmail.com>
2026-07-16 16:04:48 +0530

    remove extra file

--- src/utils/constants.js
+++
@@ -1,7 +0,0 @@
-// export const BASE_URL = "/api"
-
-// use in dev 
-export const BASE_URL = "/api"
-
-// use in production
-// export const BASE_URL = location.hostname == "localhost" ? "http://localhost:5000/api" : "/api";
